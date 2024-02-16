fbfe0b49b18db7d2efd03992f24b041d95d80857
Christian Albán <chris_alban@live.com>
2024-02-15 20:10:20 -0500

    Compile

--- src/resources/js/dist/assets/FullScreen-ae8af8bc.js
+++
@@ -1,1 +0,0 @@
-/* empty css               */import{d as o,r,c as t,a as n,o as a}from"./index-83999901.js";const m=o({__name:"FullScreen",setup(c){return(s,_)=>{const e=r("router-view");return a(),t("div",null,[n(e)])}}});export{m as default};
