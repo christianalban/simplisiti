2f338d70b588ecc57cb70cbcc39d8e341c8baaeb
Christian Albán <chris_alban@live.com>
2024-02-15 20:07:28 -0500

    Hotfix null element on autosize directive

--- src/resources/js/src/directives/auto-size-to-child.ts
+++ src/resources/js/src/directives/auto-size-to-child.ts
@@ -1,9 +1,9 @@
 export const AutoSizeToChild = {
     mounted(el: HTMLElement) {
         const firstChild = el.firstChild as HTMLElement | null;
-        const style = window.getComputedStyle(firstChild as Element);
         setTimeout(() => {
             if (firstChild) {
+                const style = window.getComputedStyle(firstChild as Element);
                 el.style.width = style?.width;
                 el.style.height = style?.height;
             }
