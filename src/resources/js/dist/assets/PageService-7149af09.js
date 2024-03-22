79d4efa669c1a6b4ef4c395c1a609acce3116743
Christian Albán <chris_alban@live.com>
2024-03-22 08:24:16 -0500

    Compile to production

---
+++ src/resources/js/dist/assets/PageService-7149af09.js
@@ -0,0 +1,1 @@
+import{b as e,M as r}from"./index-5989e792.js";import{a as n}from"./helpers-66e66eca.js";const c=async a=>await e.post("page",a),p=async()=>await e.get("page"),i=async(a,t)=>{const s=n(`page/${a}`,t);return await e.get(s)},u=async a=>await e.put(`page/${a.id}`,a),d=async a=>await e.delete(`page/${a}`),w=a=>`${r}/page/${a}/preview`;export{i as a,w as b,c,d,p as g,u};
