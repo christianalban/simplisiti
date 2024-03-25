87dec33aa429da78fb0708c7e14f3c2b2c5cb2a3
Christian Albán <chris_alban@live.com>
2024-03-25 18:01:50 -0500

    Fix absolute favicon path and load body content

--- src/resources/views/boot.blade.php
+++ src/resources/views/boot.blade.php
@@ -5,11 +5,11 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{{ $title }}</title>
-        <link rel="icon" href="sp-assets/favicon.ico">
+        <link rel="icon" href="/sp-assets/favicon.ico">
         @include('simplisiti::partials.styles')
   </head>
   <body>
-    @include('simplisiti::partials.head')
+    @include('simplisiti::partials.body')
     {!! $content !!}
     @include('simplisiti::partials.scripts')
   </body>
