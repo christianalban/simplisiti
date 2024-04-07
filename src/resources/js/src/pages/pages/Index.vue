693c65572ad2b2a02dee11cafda79546bdf8ae9c
Christian Albán <chris_alban@live.com>
2024-04-06 21:35:27 -0500

    Fix link on another tab

--- src/resources/js/src/pages/pages/Index.vue
+++ src/resources/js/src/pages/pages/Index.vue
@@ -37,9 +37,11 @@ onMounted(() => {
                             </div>
                             <div class="flex gap-2">
                                 <span>{{ $t('pages.labels.sectionsCount', { count: page.sections_count }) }}</span>
-                                <a :href="page.url" class="button small default" target="_blank">
-                                    <fa-icon icon="up-right-from-square" />
-                                </a>
+                                <div>
+                                    <a :href="page.url" class="button small default" target="_blank">
+                                        <fa-icon icon="up-right-from-square" />
+                                    </a>
+                                </div>
                             </div>
                         </a>
                     </li>
