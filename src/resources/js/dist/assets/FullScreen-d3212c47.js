5733e538afdae4bcea38e1a41c01871c043a6d87
Christian Albán <chris_alban@live.com>
2024-04-17 00:39:25 -0500

    Compile to production

---
+++ src/resources/js/dist/assets/FullScreen-d3212c47.js
@@ -0,0 +1,1 @@
+import{_ as o,r,o as c,c as n,a as t}from"./index-5f5f7cb7.js";const s={};function a(_,l){const e=r("router-view");return c(),n("div",null,[t(e)])}const f=o(s,[["render",a]]);export{f as default};
