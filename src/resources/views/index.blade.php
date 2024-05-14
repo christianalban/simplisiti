8f53d9bcbefa5b96f84ebab7811d5a3ac5fb05d5
Christian Albán <chris_alban@live.com>
2024-05-14 15:52:45 -0500

    Updated index.blade.php with new assets

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -5,8 +5,8 @@
     <link rel="icon" type="image/vnd.microsoft.icon" href="/vendor/simplisiti/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
     <title>Simplisiti Panel</title>
-    <script type="module" crossorigin src="/vendor/simplisiti/assets/index-5f5f7cb7.js"></script>
-    <link rel="stylesheet" href="/vendor/simplisiti/assets/index-954eed19.css">
+    <script type="module" crossorigin src="/vendor/simplisiti/assets/index-c7f12366.js"></script>
+    <link rel="stylesheet" href="/vendor/simplisiti/assets/index-c1cc116c.css">
   </head>
   <body>
     <div id="app"></div>
