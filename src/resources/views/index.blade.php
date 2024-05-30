efbcc03c20300ee2794e00a01bd268b984a1377a
Christian Albán <chris_alban@live.com>
2024-05-29 23:29:24 -0500

    Compile assets

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -5,8 +5,8 @@
     <link rel="icon" type="image/vnd.microsoft.icon" href="/vendor/simplisiti/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
     <title>Simplisiti Panel</title>
-    <script type="module" crossorigin src="/vendor/simplisiti/assets/index-82dfae72.js"></script>
-    <link rel="stylesheet" href="/vendor/simplisiti/assets/index-643125c5.css">
+    <script type="module" crossorigin src="/vendor/simplisiti/assets/index-94e22fa3.js"></script>
+    <link rel="stylesheet" href="/vendor/simplisiti/assets/index-98e588b2.css">
   </head>
   <body>
     <div id="app"></div>
