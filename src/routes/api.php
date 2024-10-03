2599201c0857a029ed9d0ece1adbce1043cb9cab
Christian Albán <chris_alban@live.com>
2024-10-02 20:00:15 -0500

    Add clone component api route

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,11 +3,12 @@
 use Alban\Simplisiti\Controllers;
 use Illuminate\Support\Facades\Route;
 
-// For Develop remove the `auth:sanctum` middleware
+// For Develop remove the 'auth:sanctum' middleware
 Route::prefix('api/spanel')->middleware(['api', 'auth:sanctum'])->group(function () {
     Route::controller(Controllers\ComponentController::class)->group(function () {
         Route::get('component', 'index');
         Route::get('component/{component}', 'show');
+        Route::post('component/{component}/clone', 'clone');
         Route::post('component', 'store');
         Route::put('component/{component}', 'update');
         Route::delete('component/{component}', 'destroy');
