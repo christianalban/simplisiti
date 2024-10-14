f2553982c79c38a23a385fc9414ef3e5713a1260
Christian Albán <chris_alban@live.com>
2024-10-14 16:17:19 -0500

    Allways display help

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <link rel="icon" type="image/vnd.microsoft.icon" href="/vendor/simplisiti/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
     <title>Simplisiti Panel</title>
-    <script type="module" crossorigin src="/vendor/simplisiti/assets/index-c42ec8ac.js"></script>
+    <script type="module" crossorigin src="/vendor/simplisiti/assets/index-2c6d4927.js"></script>
     <link rel="stylesheet" href="/vendor/simplisiti/assets/index-278e8699.css">
   </head>
   <body>
